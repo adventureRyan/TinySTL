2bc600ebedd0f4fcb162d9521641ead5d2174088
RyanWang <129303432+adventureRyan@users.noreply.github.com>
2024-03-21 22:37:43 +0800

    add all_of, count, find

--- hello.cc
+++ hello.cc
@@ -1,2 +1,35 @@
 #include <iostream>
 
+namespace Mystl
+{
+  template<typename InputIter, typename UnaryPredicate>
+  bool all_of(InputIter first, InputIter last, UnaryPredicate pred) {
+    for (; first != last; ++first) {
+      if (!pred(*first)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  template<typename InputIter, typename T>
+  auto count(InputIter first, InputIter last, const T &value) -> size_t
+  {
+    auto count = 0ZU;//auto会强制将类型写到后面
+    for (; first != last; ++first) {
+      if (*first == value)
+        count++;
+    }
+    return count;
+  }
+
+  //const能传递右值
+  //引用是避免开销
+  template<typename InputIter, typename T>
+  InputIter find(InputIter first, InputIter last, const T &value)
+  {
+    while (first != last && *first == value)
+      first++;
+    return first;
+  }
+}
